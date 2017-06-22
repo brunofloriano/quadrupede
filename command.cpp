3e76c2dbd924b8210963413fc1e76a88049924cd
brunofloriano <brunofloriano3@gmail.com>
2017-06-21 21:37:40 -0300

    Update command.cpp

--- command.cpp
+++ command.cpp
@@ -1,34 +1,34 @@
-#ifndef COMMAND_cpp
-#define COMMAND_cpp
-#include "include/command.h"
+#ifndef COMMAND_cpp              //Se COMMAND_cpp não está definido existe
+#define COMMAND_cpp              // Criar COMMAND_cpp (ele é um preprocessador de C		)
+#include "include/command.h"	//Se já estiver incluílo
 
-void command::DelayMicrosecondsNoSleep (int delay_us)
+void command::DelayMicrosecondsNoSleep (int delay_us)	//função atraso sem dormir (recebe delay_us, que é o tempo de atraso)
 {
     long int start_time;
     long int time_difference;
-    struct timespec gettime_now;
+    struct timespec gettime_now;	//agrupra gettime_now no registro timespec
 
-    clock_gettime(CLOCK_REALTIME, &gettime_now);
-    start_time = gettime_now.tv_nsec;		//Get nS value
-    while (1)
+    clock_gettime(CLOCK_REALTIME, &gettime_now);	//pega o tempo atual do clock e coloca no endereço de gettime_now
+    start_time = gettime_now.tv_nsec;		//Get nS value => tempo de início = # de nanosegundos passados
+    while (1)					//while infinito
     {
-        clock_gettime(CLOCK_REALTIME, &gettime_now);
-        time_difference = gettime_now.tv_nsec - start_time;
-        if (time_difference < 0)
-            time_difference += 1000000000;				//(Rolls over every 1 second)
-        if (time_difference > (delay_us * 1000))		//Delay for # nS
-            break;
+        clock_gettime(CLOCK_REALTIME, &gettime_now);	//pega o tempo atual
+        time_difference = gettime_now.tv_nsec - start_time;	//diferença = tempo_agora - tempo_início
+        if (time_difference < 0)	//se a  diferença é menor que 0 => tempo_agora < tempo_início
+            time_difference += 1000000000;				//(Rolls over every 1 second) => aumenta 1 segundo
+        if (time_difference > (delay_us * 1000))		//Delay for # nS. Se a diferença maior que o atraso
+            break;						//Quebra o while
     }
 }
 
-int command::read_pos(dynamixel::PortHandler *portHandler, dynamixel::PacketHandler *packetHandler, uint8_t id)
-{
-
-    uint8_t dxl_error = 0;
-    int     dxl_comm_result = COMM_TX_FAIL;
-    int16_t value16   = 0;
+int command::read_pos(dynamixel::PortHandler *portHandler, dynamixel::PacketHandler *packetHandler, uint8_t id) //comando ler posição
+{				//entradas: porthandler, packethandler, uint8_t e id
+				//Dynamixel = empresa de servomotores
+    uint8_t dxl_error = 0; //unsigned char dexl_error = 0 => categoria de variável que vai até 255
+    int     dxl_comm_result = COMM_TX_FAIL; //COMM_TX_FAIL é um parâmetro do servomotor definido como -1001
+    int16_t value16   = 0; //int16_t vai de -32,768 to 32,767
     do{
-        dxl_comm_result=packetHandler->read2ByteTxRx(portHandler, id, 36,(uint16_t*)&value16,&dxl_error);  // Endereço p/posiçao atual 36,37
+        dxl_comm_result=packetHandler->read2ByteTxRx(portHandler, id, 36,(uint16_t*)&value16,&dxl_error);  // Endereço p/posiçao atual 36,37 
         if (dxl_comm_result == COMM_SUCCESS)
         {
             if (dxl_error != 0) packetHandler->printRxPacketError(dxl_error);
